880f585eb35079dfbacfc15661dbdc23ba264ac8
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-07-14 16:43:41 -0500

    Added cucumber

--- features/step_definitions/welcome_page_steps.rb
+++
@@ -1,7 +0,0 @@
-When(/^I am on the homepage$/) do
-  visit '/api/v1/welcome'
-end
-
-Then(/^I should see hello world text$/) do
-  expect(page).to have_content "Hello world"
-end
--- features/welcome_page.feature
+++
@@ -1,5 +0,0 @@
-Feature: Welcome page
-
-  Scenario: Check weclome controller
-    When I am on the homepage
-    Then I should see hello world text
