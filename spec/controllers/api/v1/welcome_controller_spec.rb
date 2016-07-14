a7e996554524547e4fe030eb0a3f9964b4658714
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-07-14 15:59:34 -0500

    Empy commit

--- spec/controllers/api/v1/welcome_controller_spec.rb
+++ spec/controllers/api/v1/welcome_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Api::V1::WelcomeController, :type => :controller do
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code" do
       get :index
+      
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
