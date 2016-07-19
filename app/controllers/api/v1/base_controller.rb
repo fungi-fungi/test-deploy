9760956592bc06adf34a608da0c1fb440d26b8cb
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-07-19 12:30:56 -0500

    Tests for base controller

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,3 +1,3 @@
-class Api::V1::BaseController < ApplicationController
-
+class Api::V1::BaseController < SecuredResourceController
+  rescue_from ActiveRecord::RecordNotFound, :with => :not_found
 end
---
+++ spec/controllers/api/v1/base_controller_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::BaseController, :type => :controller do
+  describe "Request page without correct crededntials" do
+    
+  end
+end
