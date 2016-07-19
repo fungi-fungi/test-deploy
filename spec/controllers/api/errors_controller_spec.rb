2dbad34d50b6243a9c2a385cace3753ad403b3ac
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-07-19 12:31:25 -0500

    Tests for erorrs controller

--- spec/controllers/api/errors_controller_spec.rb
+++ spec/controllers/api/errors_controller_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Api::V1::ErrorsController, :type => :controller do
   describe "Request unexisting path" do
-    it "responds with error message" do
+    it "gets error message" do
       expect(get: '/bla-bla').to route_to(controller: 'errors', action: 'routing', unmatched_route: 'bla-bla')
     end
   end
