27aa1ae8f733377f5c5de778b63755c87be704a7
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-07-19 10:26:23 -0500

    Login feature

---
+++ features/login.feature
@@ -0,0 +1,7 @@
+Feature: Login
+
+  @jascript
+  Scenario: Login
+  Given "Iurii" has a account in the system
+  When "Iurii" entenrs correct credentials
+  Then he can see his profile
---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,11 @@
+Given(/^"([^"]*)" has a account in the system$/) do |arg1|
+  true
+end
+
+When(/^"([^"]*)" entenrs correct credentials$/) do |arg1|
+  true
+end
+
+Then(/^he can see his profile$/) do
+  true
+end
