2f454d39ec3a7a9d2e348f603600e6f572228e3a
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-07-20 13:45:43 -0500

    Logout scenario

--- features/login.feature
+++ features/login.feature
@@ -5,3 +5,10 @@ Feature: Login
     Given I am on main page
     When I enter correct credentials
     Then I can see my profile
+
+
+  @jascript
+  Scenario: Logout
+    Given I am logged in
+    When I logout
+    Then I am logged out
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -12,3 +12,16 @@ end
 Then(/^I can see my profile$/) do
   expect(find('.marketing').find('h1')).to have_content('It works 3')
 end
+
+Given(/^I am logged in$/) do
+  step "I am on main page"
+  step "I enter correct credentials"
+end
+
+When(/^I logout$/) do
+  visit 'http://localhost:9000/#!/logout'
+end
+
+Then(/^I am logged out$/) do
+   expect(find('h1')).to have_content('Log in')
+end
