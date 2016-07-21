9e4636ea21479522c4636fa826b17f888da9c3f5
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-07-21 16:12:21 -0500

    Stock item routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :welcome, as: :welcome, controller: :welcome, only: [:index, :options]
+      resources :stock_items, only: [:index, :show]
     end
   end
 
