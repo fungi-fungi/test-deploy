bdeafbc1651014e5a3ffd62c8a3a27dffdd508c4
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-08-03 10:57:25 -0500

    Added dependencies to existing models

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,7 @@ class Account < ActiveRecord::Base
   self.table_name = 'salesforce.account'
   has_many :stock_items, primary_key: "sfid", foreign_key: "i_m__to_account__c"
   has_many :configurations, primary_key: "sfid", foreign_key: "i_m__to_account__c"
+  has_many :order_requests, primary_key: "sfid", foreign_key: "i_m__account__c"
 
   validates :name, :sfid, presence: true
 end
--- app/models/bom.rb
+++ app/models/bom.rb
@@ -1,5 +1,7 @@
 class Bom < ActiveRecord::Base
   self.table_name = 'salesforce.i_m__bom__c'
+  has_many :item_entities, primary_key: "sfid", foreign_key: "i_m__to_bom__c"
+
 
   validates :name, :sfid, presence: true
 end
