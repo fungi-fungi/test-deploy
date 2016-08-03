9d9b216c87310c245a51638f6f62d0cd1c1c16cf
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-08-03 11:27:20 -0500

    View all orders scenario

--- features/order.feature
+++ features/order.feature
@@ -6,8 +6,15 @@ Feature: Ordering
     And I have 10 items in my inventory
     And I have 2 avaliable configuration
 
-  @jascript
+  @javascript
   Scenario: I can see all configurations
     Given I am on my profile page
     When I go to Configuration page
     Then I can see 2 avaliable configurations
+
+  @javascript
+  Scenario: I can see all my previous orders
+    Given I have 5 previous orders
+    And I am on my profile page
+    When I go to Orders page
+    Then I can see 5 orders
--- features/step_definitions/order_steps.rb
+++ features/step_definitions/order_steps.rb
@@ -11,3 +11,18 @@ Then(/^I can see (\d+) avaliable configurations$/) do |arg1|
   expect(page).to have_content(Configuration.first.name)
   expect(find('#configurations-table').all('tr').size).to eq(arg1.to_i)
 end
+
+Given(/^I have (\d+) previous orders$/) do |arg1|
+  FactoryGirl.create_list(:order_with_sfid, arg1.to_i, account: Account.where(sfid: '0013600000NSyCRAA1').first)
+end
+
+When(/^I go to Orders page$/) do
+  find("a#orders-page").click
+  expect(page).to have_content("This is the Orders view.")
+end
+
+Then(/^I can see (\d+) orders$/) do |arg1|
+  page.save_screenshot('screenshot.png')
+  expect(page).to have_content(OrderRequest.first.sfid)
+  expect(find('#orders-table').all('tr').size).to eq(arg1.to_i)
+end
