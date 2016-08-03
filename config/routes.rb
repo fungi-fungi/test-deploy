c588e953ef8110bbf96b6def49ee042930fe9db0
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-08-03 11:03:38 -0500

    Routes for orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :welcome, as: :welcome, controller: :welcome, only: [:index, :options]
-      resources :stock_items, only: [:index, :show]
+      resources :welcome, as: :welcome, controller: :welcome, only: [:index]
+      resources :stock_items, only: [:index]
       resources :configurations, only: [:index, :show]
+      resources :orders, only: [:index, :show, :create]
     end
   end
 
