28ac67138c48ed72bd964a149ff4a93fc65854ec
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-08-03 11:26:58 -0500

    Added factory with sfid

--- spec/factories/order_requests.rb
+++ spec/factories/order_requests.rb
@@ -4,5 +4,11 @@ FactoryGirl.define do
     f.i_m__start_date__c    { Faker::Time.between(10.days.ago, 5.days.ago) }
     f.i_m__end_date__c      { Faker::Time.between(4.days.ago, Date.today) }
     association             :account
+
+    trait :order_with_sfid do |f|
+      f.sfid                { Faker::Number.number(10) }
+    end
+
+    factory :order_with_sfid, traits: [:order_with_sfid]
   end
 end
