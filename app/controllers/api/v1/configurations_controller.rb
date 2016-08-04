92178a1efecff78e06984d31efe7992996e08e75
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-08-04 08:53:24 -0500

    Show action for configuration controller

--- app/controllers/api/v1/configurations_controller.rb
+++ app/controllers/api/v1/configurations_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::ConfigurationsController < Api::V1::BaseController
   def index
     paginate json: client.configurations, each_serializer: Api::V1::ConfigurationSerializer
   end
+
+  def show
+    @configuration = client.configurations.where(id: params[:id]).first
+    render json: @configuration, serializer: Api::V1::ConfigurationDetailsSerializer, include: '**'
+  end
 end
