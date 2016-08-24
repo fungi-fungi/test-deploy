8355269865b70b9adad9ee9152abf5d213c82d0f
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-08-24 10:19:32 -0500

    Correct relationships m-to-m between Bom and Item

--- app/models/bom.rb
+++ app/models/bom.rb
@@ -1,6 +1,7 @@
 class Bom < ActiveRecord::Base
   self.table_name = 'salesforce.i_m__bom__c'
   
+  has_many :items, through: :item_entities
   has_many :item_entities, primary_key: "sfid", foreign_key: "i_m__to_bom__c"
   has_one :configuration, primary_key: "sfid", foreign_key: "i_m__to_bom__c"
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ActiveRecord::Base
   self.table_name = 'salesforce.i_m__item__c'
   
   has_many :request_boms, through: :request_entities
+  has_many :boms, through: :item_entities
   has_many :request_entities, primary_key: 'sfid', foreign_key: 'i_m__to_item__c'
   has_many :item_entities, primary_key: 'sfid', foreign_key: 'i_m__to_item__c'
   has_many :stock_items, primary_key: "sfid", foreign_key: "i_m__to_item__c"
