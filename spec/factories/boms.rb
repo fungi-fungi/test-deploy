4e975f484e9b64997a707dfc09658c7b9cc62c6e
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-08-30 16:42:45 -0500

    Additional factories

--- spec/factories/boms.rb
+++ spec/factories/boms.rb
@@ -6,5 +6,40 @@ FactoryGirl.define do
     factory :bom_with_entities do
       after(:create) { |bom| create_list(:item_entity, 10, bom: bom) }
     end
+
+    factory :bom_for_exisiting_items_random do
+
+      transient do
+        existing_items []
+      end
+
+      transient do
+        amount_of_items 0
+      end
+
+      after(:create) do |bom, evaluator|
+
+        evaluator.amount_of_items.times do |item|
+          create(:item_entity, bom: bom, item: evaluator.existing_items.sample )
+        end
+
+      end 
+    end
+
+    factory :bom_for_exisiting_items do
+
+      transient do
+        existing_items []
+      end
+
+      after(:create) do |bom, evaluator|
+
+        evaluator.existing_items.each do |item|
+          create(:item_entity, bom: bom, item: item )
+        end
+
+      end 
+    end
+    
   end
 end
--- spec/factories/request_bom.rb
+++ spec/factories/request_bom.rb
@@ -6,5 +6,39 @@ FactoryGirl.define do
       after(:create) { |request_bom| create_list(:request_entity, 10, request_bom: request_bom) }
     end
 
+    factory :request_bom_for_exisiting_items_random do
+
+      transient do
+        existing_items []
+      end
+
+      transient do
+        amount_of_items 0
+      end
+
+      after(:create) do |request_bom, evaluator|
+
+        evaluator.amount_of_items.times do |item|
+          create(:request_entity, request_bom: request_bom, item: evaluator.existing_items.sample )
+        end
+
+      end
+    end
+
+    factory :request_bom_for_exisiting_items do
+
+      transient do
+        existing_items []
+      end
+
+      after(:create) do |request_bom, evaluator|
+
+        evaluator.existing_items.each do |item|
+          create(:request_entity, request_bom: request_bom, item: item )
+        end
+
+      end 
+    end
+
   end
 end
