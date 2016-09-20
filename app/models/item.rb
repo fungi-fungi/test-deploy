351cb537561f0b134c212fb8b755da9f91a158f0
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-09-20 10:29:56 -0500

    Fixed wrong relationship direction item - category

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ActiveRecord::Base
   has_many :request_entities, primary_key: 'sfid', foreign_key: 'i_m__to_item__c'
   has_many :item_entities, primary_key: 'sfid', foreign_key: 'i_m__to_item__c'
   has_many :stock_items, primary_key: "sfid", foreign_key: "i_m__to_item__c"
-  belongs_to :category, primary_key: "i_m__externalid__c", foreign_key: "i_m__category__c__i_m__externalid__c"
+  belongs_to :category, primary_key: "sfid", foreign_key: "i_m__to_category__c"
 
   validates_presence_of :sfid, :name, :category, :createddate
 end
--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :category do |f|
-    f.sfid                { Faker::Number.number(10) }
-    f.name                { Faker::Commerce.product_name }
-    f.i_m__externalid__c  { Faker::Number.number(10) }
+    f.sfid { Faker::Number.number(10) }
+    f.name { Faker::Commerce.product_name }
   end
 end
