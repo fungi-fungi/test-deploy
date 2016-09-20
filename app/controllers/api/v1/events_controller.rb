6e4f06b75f4beb6b2d3cc882220e200cc09fc7e3
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-09-20 15:11:15 -0500

    Events controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,4 +4,12 @@ class Api::V1::EventsController < Api::V1::BaseController
     paginate json: Event.all, each_serializer: Api::V1::EventSerializer
   end
 
+  def show
+    render json: Event.find(params[:id]), serializer: Api::V1::EventSerializer
+  end
+
+  def search
+    paginate json: Event.search_by_name(params[:name]), each_serializer: Api::V1::EventSerializer
+  end
+
 end
--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -7,6 +7,28 @@ FactoryGirl.define do
     f.i_m__end_date__c            { Faker::Time.between(10.days.ago, Date.today) }
     association                   :account, factory: :account_venue
 
+    factory :event_name_overlap do
+
+      transient do
+        overlap_amount 0
+      end
+
+      transient do
+        non_overlap_amount 0
+      end
+
+      after(:create) do |event, evaluator|
+        evaluator.overlap_amount.times do
+          create :event, name: "#{Faker::Number.number(3)}#{event.name}"
+        end
+
+        evaluator.non_overlap_amount.times do
+          create :event, name: "#{Faker::Company.name}.gsub(event.name, '')"
+        end
+      end
+      
+    end
+
     factory :event_overlap do 
       
       transient do
@@ -18,60 +40,56 @@ FactoryGirl.define do
       end
 
       after(:create) do |event, evaluator|
+        
+        evaluator.overlap_amount.times do
 
-        evaluator.overlap_amount.times do |x|
-          
-          # Left
-          create(
-            :event,
+          args_left = {
+            account: event.account, 
+            i_m__start_date__c: Faker::Time.between(event.i_m__start_date__c - 7.day, event.i_m__start_date__c - 1.day),
+            i_m__end_date__c: Faker::Time.between(event.i_m__start_date__c + 1.day, event.i_m__end_date__c - 1.day)
+          }
+
+          args_in = {
             account: event.account,
             i_m__start_date__c: Faker::Time.between(event.i_m__start_date__c - 7.day, event.i_m__start_date__c - 1.day),
             i_m__end_date__c: Faker::Time.between(event.i_m__start_date__c + 1.day, event.i_m__end_date__c - 1.day)
-          )
+          }
 
-          # In
-          create(
-            :event,
-            account: event.account,
-            i_m__start_date__c: Faker::Time.between(event.i_m__start_date__c + 1.day, event.i_m__start_date__c + 2.day),
-            i_m__end_date__c: Faker::Time.between(event.i_m__end_date__c - 2.day, event.i_m__end_date__c - 1.day)
-          )
-
-          # Right
-          create(
-            :event,
+          args_right = {
             account: event.account,
             i_m__start_date__c: Faker::Time.between(event.i_m__start_date__c + 1.day, event.i_m__end_date__c - 1.day),
             i_m__end_date__c: Faker::Time.between(event.i_m__end_date__c + 1.day, event.i_m__end_date__c + 5.day)
-          )
+          }
 
-          # Out
-          create(
-            :event,
+          args_out = {
             account: event.account,
             i_m__start_date__c: Faker::Time.between(event.i_m__start_date__c - 5.day, event.i_m__start_date__c - 1.day),
             i_m__end_date__c: Faker::Time.between(event.i_m__end_date__c + 1.day, event.i_m__end_date__c + 5.day)
-          )
+          }
 
+          create :event, args_right
+          create :event, args_left
+          create :event, args_in
+          create :event, args_out
         end
 
-        evaluator.non_overlap_amount.times do |x|
+        evaluator.non_overlap_amount.times do
           
-          # Before
-          create(
-            :event,
+          args_before = {
             account: event.account,
             i_m__start_date__c: Faker::Time.between(event.i_m__start_date__c - 30.day, event.i_m__start_date__c - 15.day),
             i_m__end_date__c: Faker::Time.between(event.i_m__start_date__c - 14.day, event.i_m__start_date__c - 2.day)
-          )
+          }
 
-          # After
-          create(
-            :event,
+          args_after = {
             account: event.account,
             i_m__start_date__c: Faker::Time.between(event.i_m__end_date__c + 2.day, event.i_m__end_date__c + 5.day),
             i_m__end_date__c: Faker::Time.between(event.i_m__end_date__c + 7.day, event.i_m__end_date__c + 15.day)
-          )
+          }
+
+        create :event, args_before
+        create :event, args_after
+
         end
 
       end
