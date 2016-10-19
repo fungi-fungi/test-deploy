a110baa51ab3012b7c00221b9fff198052169c67
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-10-19 11:44:13 -0500

    Fixed serialization for request entity

--- app/serializers/api/v1/request_entity_serializer.rb
+++ app/serializers/api/v1/request_entity_serializer.rb
@@ -1,7 +1,7 @@
 class Api::V1::RequestEntitySerializer < ActiveModel::Serializer
   attributes :id, :sfid, :name, :amount, :createddate
   
-  belongs_to :item
+  belongs_to :item, serializer: Api::V1::ItemSerializer
   
   def amount
     object.i_m__amount__c
