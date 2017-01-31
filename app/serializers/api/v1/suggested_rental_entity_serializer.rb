35faad75e7b68c86ebd46eb0df92fec8355552a8
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2017-01-31 17:23:34 +0200

    Fix in serializer

--- app/serializers/api/v1/suggested_rental_entity_serializer.rb
+++ app/serializers/api/v1/suggested_rental_entity_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::SuggestedRentalEntitySerializer < ActiveModel::Serializer
   attributes :id, :sfid
 
-  has_one: item, serializer: Api::V1::ItemDetailsSerializer
+  has_one :item, serializer: Api::V1::ItemDetailsSerializer
 end
